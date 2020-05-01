785d9c148014aeff0ea391649cbf28f64639665e
Danielle Ellis <thedanielleellis@gmail.com>
2020-05-01 05:03:53 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,3 +5,17 @@ class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
+
+  
+require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
+
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
