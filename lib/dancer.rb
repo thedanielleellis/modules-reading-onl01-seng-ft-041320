4a1f74ca31c9e138b0b738651bf15e52dd7a3ee2
Danielle Ellis <thedanielleellis@gmail.com>
2020-05-01 05:01:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,15 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-
-class Dancer 
-    include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-end 
+require_relative './fancy_dance.rb'
 
 class Dancer
   extend FancyDance::ClassMethods
