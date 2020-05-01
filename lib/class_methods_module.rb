aaad38428ba2070f2fbced9543ffb288505898db
Danielle Ellis <thedanielleellis@gmail.com>
2020-05-01 04:41:04 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,4 +3,13 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
+  
+  module MetaDancing
+ 
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
+
+
 end
